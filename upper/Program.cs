c0daa85434e0290923073e0a8f6cbdb39fa39d0a
azadei <azadei@code.edu.az>
2023-07-10 16:09:29 +0400

    1

---
+++ upper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+
+namespace upper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "java".ToUpper();
+
+            string name2 = name.ToUpper();
+
+            Console.WriteLine(name2.Reverse());
+        }
+    }
+}
